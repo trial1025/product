1fa79508152d69d20e459bb29ddaf47ba96bc134
Rood <a2973799@gmail.com>
2024-02-17 16:19:15 +0800

    修正

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import routeUsers from './routes/users.js'
+import routeProducts from './routes/products.js'
+import routeOrders from './routes/orders.js'
 import { StatusCodes } from 'http-status-codes'
+import './passport/passport.js'
 
 const app = express()
 
 app.use(cors({
+  // origin = 請求的來源
+  // callback(錯誤, 是否允許)
   origin (origin, callback) {
     if (origin === undefined || origin.includes('github.io') || origin.includes('localhost')) {
       callback(null, true)
@@ -32,9 +37,18 @@ app.use((_, req, res, next) => {
 })
 
 app.use('/users', routeUsers)
+app.use('/products', routeProducts)
+app.use('/orders', routeOrders)
 
-app.listen(process.env.Port || 4000, async () => {
-  console.log('伺服器平安啟動')
+app.all('*', (req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: '找不到'
+  })
+})
+
+app.listen(process.env.PORT || 4000, async () => {
+  console.log('伺服器啟動')
   await mongoose.connect(process.env.DB_URL)
   console.log('資料庫連線成功')
 })
